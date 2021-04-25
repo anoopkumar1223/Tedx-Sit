f0cf6374cc7bb6caa67c1aa0dcefd663704d9961
Anoopkumar1223 <anoopshavi55@gmail.com>
2021-04-25 23:52:47 +0530

    initial commit

---
+++ tedx_sit/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:tedx_sit/resources/router.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      onGenerateRoute: MyRouter.generateRoute,
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TEDX'),
+      ),
+      body: Center(
+        child: Text(
+          'Welcome',
+        ),
+      ),
+    );
+  }
+}
